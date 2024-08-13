1af6d30d85065e93867d2b13609f7507243f158a
Michael Gatewood <michael@coincloudnv.com>
2024-08-13 11:11:55 -0700

    Display email address on Account page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,9 +1,13 @@
 <script>
+
+    export let data;
+
 </script>
 
 <div class="container">
-        <h1>Account</h1>
-        <a data-sveltekit-preload-data="off" href="/logout" class="button href-button">Log Out</a>
+    <h1>Account</h1>
+    <h4 class="uppercase">{data.user.email}</h4>
+    <a data-sveltekit-preload-data="off" href="/logout" class="button href-button">Log Out</a>
 </div>
 
 <style lang="postcss">
