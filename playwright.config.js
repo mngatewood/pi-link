9600337ef8369118438144614d87eccbef0a28eb
Michael Gatewood <michael@coincloudnv.com>
2024-08-14 19:44:31 -0700

    Add projects to playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,52 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
-};
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] },
+		},
 
-export default config;
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] },
+		},
+
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] },
+		},
+
+		/* Test against mobile viewports. */
+		{
+			name: 'Mobile Chrome',
+			use: { ...devices['Pixel 5'] },
+		},
+		{
+			name: 'Mobile Safari',
+			use: { ...devices['iPhone 12'] },
+		},
+
+		/* Test against branded browsers. */
+		{
+			name: 'Microsoft Edge',
+			use: {
+				...devices['Desktop Edge'],
+				channel: 'msedge'
+			},
+		},
+		{
+			name: 'Google Chrome',
+			use: {
+				...devices['Desktop Chrome'],
+				channel: 'chrome'
+			},
+		},	]
+});
+
