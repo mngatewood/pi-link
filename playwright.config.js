5fa05067dd6d0a8dc5024c81b2a087ec3b19f5b2
Michael Gatewood <michael@coincloudnv.com>
2024-08-15 20:09:17 -0700

    Add db teardown for all tests and projects

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config'
+console.log(process.env.VITE_DB_URL);
 
 export default defineConfig({
 	webServer: {
@@ -9,28 +11,38 @@ export default defineConfig({
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	projects: [
 		{
+			name: 'cleanup db',
+			testMatch: /global\.teardown\.js/,
+		},
+		{
 			name: 'chromium',
 			use: { ...devices['Desktop Chrome'] },
+			dependencies: [ 'cleanup db' ],
 		},
 
 		{
 			name: 'firefox',
 			use: { ...devices['Desktop Firefox'] },
+			dependencies: ['cleanup db'],
 		},
 
 		{
 			name: 'webkit',
 			use: { ...devices['Desktop Safari'] },
+			dependencies: ['cleanup db'],
 		},
 
 		/* Test against mobile viewports. */
 		{
 			name: 'Mobile Chrome',
 			use: { ...devices['Pixel 5'] },
+			dependencies: ['cleanup db'],
 		},
+
 		{
 			name: 'Mobile Safari',
 			use: { ...devices['iPhone 12'] },
+			dependencies: ['cleanup db'],
 		},
 
 		/* Test against branded browsers. */
@@ -40,13 +52,16 @@ export default defineConfig({
 				...devices['Desktop Edge'],
 				channel: 'msedge'
 			},
+			dependencies: ['cleanup db'],
 		},
+
 		{
 			name: 'Google Chrome',
 			use: {
 				...devices['Desktop Chrome'],
 				channel: 'chrome'
 			},
+			dependencies: ['cleanup db'],
 		},	]
 });
 
---
+++ tests/global.teardown.js
@@ -0,0 +1,45 @@
+import { test as teardown } from '@playwright/test';
+import PocketBase from 'pocketbase';
+const pb = new PocketBase("http://127.0.0.1:8090");
+
+const getAllUserIds = async () => {
+    const allUsers = await pb.collection('users').getFullList({});
+    return allUsers.map(user => user.id)
+}
+
+const deleteAllUsers = async () => {
+    const userIds = await getAllUserIds();
+    await Promise.all(userIds.map(async (userId) => {
+        console.log("userId", userId)
+        await pb.collection('users').delete(userId)
+    }));
+}
+
+const getAllGameIds = async () => {
+    const allGames = await pb.collection('games').getFullList({});
+    return allGames.map(game => game.id)
+}
+
+const deleteAllGames = async () => {
+    const gameIds = await getAllGameIds();
+    await Promise.all(gameIds.map(async (gameId) => {
+        console.log("gameId", gameId)
+        await pb.collection('games').delete(gameId)
+    }));
+}
+
+teardown('delete database', async ({ }) => {
+    console.log('deleting test database...');
+
+    const allGames = await getAllGameIds();
+    const allUsers = await getAllUserIds();
+
+    if (allGames.length) {
+        await deleteAllGames();
+    }
+
+    if (allUsers.length) {
+        await deleteAllUsers();
+    }
+
+});
