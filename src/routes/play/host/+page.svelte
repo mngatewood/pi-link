46a7d71f7b860c850dfcc04a5c4d30bb7eafd359
Michael Gatewood <michael@coincloudnv.com>
2024-08-19 15:42:45 -0700

    Add tests for start game

--- src/routes/play/host/+page.svelte
+++ src/routes/play/host/+page.svelte
@@ -7,8 +7,8 @@
         <h1>Host a New Game</h1>
         <h4 class="px-4">Share the following code to allow other detectives to join.</h4>
     </div>
-    <div class="notepad">
-        <h2>{data.code?.toString().padStart(4,'0')}</h2>
+    <div data-testid="host-code-notepad" class="notepad">
+        <h2 id="host-code">{data.code?.toString().padStart(4,'0')}</h2>
     </div>
     <h4 class="mb-8">Once all participants have the code, press the Continue button to proceed to the lobby.</h4>
     <div class="flex w-full">
