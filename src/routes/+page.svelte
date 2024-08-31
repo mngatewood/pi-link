838a2fcc63c14b82a5004c05b670b8884e72c1a8
Michael Gatewood <michael@coincloudnv.com>
2024-08-30 17:14:51 -0700

    Add overview to home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,9 @@
 
 <div class="container">
 	<div>
-		<h1>Welcome</h1>
-		<h4>
-			Please
-			<a data-testid="register-inline" class="href-inline" href="/register">register</a>
-			or
-			<a data-testid="login-inline" class="href-inline" href="/login">login</a>.
-		</h4>
+		<h1>Welcome!</h1>
+		<p class="overview">PI Link is a digital reimplementation of the board game <em>Detective Club</em> published in 2018.  In each round, one of the players &mdash; the Informant &mdash; secretly teams up with another &mdash; the Conspirator &mdash; and tries to make them guess a secret word using two illustrated cards.  Other players are Detectives, who also know the word, but don&rsquo;t know the identities of each other.  Detectives have to find out who the Conspirator is, making sure they don&rsquo;t get accused by their fellow players!</p>
+		<p class="overview">PI Link can be used as a companion app to the original game or can be played as a stand-alone game.  Each player just needs a mobile device and 25 unique illustrated cards, such as those provided in other popular board games like <em>Dixit</em> and <em>Mysterium</em>.  To get started, simply create an account at the <a data-testid="register-inline" class="href-inline" href="/register">Register</a> page, then login at the <a data-testid="login-inline" class="href-inline" href="/login">Login</a> page.  The app provides context-specific tips along the way to teach you the game.  If you wish to review the official rules for Detective Club, you can download them <a class="href-inline" href="https://cdn.1j1ju.com/medias/92/14/39-detective-club-rulebook.pdf">here</a>.</p>
 	</div>
 	<div class="buttons-container w-full">
 		<a data-testid="register-button" class="href-button" href="/register">Register</a>
